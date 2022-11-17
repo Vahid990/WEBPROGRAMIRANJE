efef09e4b6913874802726e95140ed47c2865815
Vahid990 <resovic59@gmail.com>
2022-11-17 12:05:27 +0100

    Initial commit

---
+++ JavaScript/app.js
@@ -0,0 +1,21 @@
+//alert("Hello")
+console.log("Hello From Console!")
+//document.write("Pozdrav")
+// komentar
+console.log(5+5);
+
+
+
+
+// Tipovi Podataka
+// 1.String -> Neki Text
+// 2.Number -> Broj
+// 3.Boolean -> logicna vrednost
+// 4.Undefined -> nema vrednost
+// Undefined Primer
+var x;
+console.log(x)
+// 5.Null
+// primer
+var y=null;
+console.log(y)
