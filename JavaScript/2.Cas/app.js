1d11dcc125a679f7cb66598bc17e8e9af28812cc
Vahid990 <resovic59@gmail.com>
2022-11-19 12:24:31 +0100

    Slozeni Tipovi Podataka

---
+++ JavaScript/2.Cas/app.js
@@ -0,0 +1,53 @@
+//Objekat
+var bmw ={boja: "bela",model:"x6"};
+console.log(bmw)
+
+var osoba = {
+    ime:"Neko",
+    godine:25,
+    zanimanje:"programer",
+    drzavljanstvo:"Nemac"
+};
+
+//Na ovaj nacin pristupamo propertijima u objektu
+var dajGodine = osoba.prezime
+// Moze i ovako
+console.log(osoba.godine);
+
+var automobili = ["BMW","Audi","Mercedes"];
+// var car = cars.length
+// var car = cars[1];
+// console.log(car)
+// Tipovi podataka
+// Slozeni tipovi podataka
+// Objekat
+// Niz
+// Funkcija
+
+// Aritmeticki Operatori
+console.log(5+5);
+console.log(5-5);
+console.log(5*5);
+console.log(5 /5)
+
+// = dodeljuje vrednost
+// == uporedjuje samo vrednost bez obzira na tip
+// === uporedjuje i tip i vrednost
+var x = 5
+var y = "5"
+console.log(y === x)
+// Nije Moguce Racunati sa stringom i number-om
+// var x = 5;
+// var y = "5";
+// console.log(x - y)
+var cars=[];
+var cars = [
+cars[0] = "BMW",
+cars[1] = "Audi",
+cars[3] = "Mercedes"
+]
+console.log (cars)
+
+
+const fruits = ["Banana","Apple","Orange","Mango"];
+console.log(fruits.sort())
