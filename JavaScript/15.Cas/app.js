47ec84892ae1127cbda1de61b563207e7d30637d
Vahid990 <resovic59@gmail.com>
2023-01-07 10:07:54 +0100

    button's

---
+++ JavaScript/15.Cas/app.js
@@ -0,0 +1,21 @@
+// var listColor = function() {
+//   var list = document.getElementsByTagName('li');
+function klik(){
+  for (var i = 0, l = list.length; i < l; i++) {
+  list[i].style.color = 'red','blue';
+  }
+};
+
+// listColor();
+// let paragraf = (document.getElementByid("numbers").style.color = "blue");
+// const btn = document.getElementById('btn');
+
+// let index = 0;
+
+// const colors = ['salmon', 'green', 'blue', 'purple'];
+
+// btn.addEventListener('click', function onClick() {
+// btn.style.backgroundColor = colors[index];
+// btn.style.color = 'white';
+// index = index >= colors.length - 1 ? 0 : index + 1;
+// });
